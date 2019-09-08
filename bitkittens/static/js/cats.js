6cc90b2ae888de1153bed040e9dd1e4dc3d6da87
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-09-08 14:24:29 -0400

    Created final assignment

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+const url = "https://eric-deploy-test.herokuapp.com/cats";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".summon-cats").addEventListener("click", () => {
+    axios
+      .get(`${url}`)
+      .then(response => {
+        const {data} = response;
+        data.cats.forEach((cat, index) => {
+          const image = document.createElement("img");
+          image.setAttribute("src", cat.photo);
+          image.setAttribute("alt", `Photo of ${cat.name}`);
+          const element = document.querySelector("#cat" + (index + 1));
+          element.appendChild(image);
+        });
+      })
+      .catch(error => console.log(error));
+  });
 });
